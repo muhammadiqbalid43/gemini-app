d04c4bb279e3d320bb2b7693f54b261b1270927a
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-08-31 11:14:15 +0700

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* HEADER */}
+        <header className="flex gap-4 p-4 border-b">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </header>
+
+        {/* CONTENT */}
+        <main className="flex-1 p-4">{children}</main>
       </body>
     </html>
   );
